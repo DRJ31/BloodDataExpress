dd7a3ee0d23531276fd1f046b9ebb5b07e6c7df9
dengrenjie <dengrenjie31@gmail.com>
2021-04-13 10:25:47 +0800

    Add environment variable in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ rm -rf application/blood
 mkdir application/blood
 tar xvf project.tar.xz -C application/blood
 cd application/blood
+export PATH=/usr/local/node/bin:$PATH
 yarn install
 pm2 start build/app.js
