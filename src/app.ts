6510e31eb8285a59ead99920944b8076b6624aaf
dengrenjie <dengrenjie31@gmail.com>
2021-05-17 17:25:09 +0800

    Try to fix login bug

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 1000 * 60 * 60,
-        secure: true,
-        sameSite: "strict"
+        secure: true
     }
 }));
 
